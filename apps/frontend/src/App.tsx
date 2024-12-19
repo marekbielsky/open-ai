30ad35fe56ebcb518f3f913e58c3e0f26b1030ab
marekbielsky <marekbielski@yahoo.com>
2024-12-19 11:30:49 +0100

    connect frontend with api

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,33 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useFetch } from './hooks/useFetch'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, loading, error } = useFetch<string>('/api')
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && <p>Message from backend: {data}</p>}
     </>
   )
 }
